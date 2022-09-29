0e681445f6d2e3deddb7e8169b4e2fd5c5b0e462
Charles Miller <charles.miller.0412@gmail.com>
2022-09-29 13:50:53 -0500

    add search to zustand

--- appStore.tsx
+++ appStore.tsx
@@ -1,5 +1,8 @@
 import create from "zustand";
 
-const useCountryStore = create((set) => ({}));
+const useCountryStore = create((set) => ({
+    search: "",
+    updateSearch: (key: any) => set({ search: key }),
+}));
 
 export default useCountryStore;
