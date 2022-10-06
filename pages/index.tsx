db4f833f084cba32d8240bb5437568b18e750faf
Charles Miller <charles.miller.0412@gmail.com>
2022-10-06 02:33:11 -0500

    add back to top button

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,9 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import Navbar from "../components/navbar";
 import SearchBar from "../components/SearchBar/searchBar";
 import dynamic from "next/dynamic";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowUp } from "@fortawesome/free-solid-svg-icons";
 
 const Home: NextPage = () => {
     const Countries = dynamic(
@@ -43,12 +44,19 @@ const Home: NextPage = () => {
 
             <main
                 className={
-                    "relative w-full h-screen flex flex-col py-[10.4rem] px-[1.6rem] md:p-[8rem] bg-lightGray dark:bg-veryDarkBlueD transition-all text-veryDarkBlueL dark:text-white overflow-x-hidden"
+                    "transition-all scroll-smooth relative w-full h-screen flex flex-col py-[10.4rem] px-[1.6rem] md:p-[8rem] bg-lightGray dark:bg-veryDarkBlueD text-veryDarkBlueL dark:text-white overflow-x-hidden"
                 }
             >
                 <Navbar />
                 <SearchBar />
                 <Countries />
+                <a
+                    href="#navbar"
+                    className="fixed bottom-12 left-8 text-white text-base flex flex-col justify-center items-center"
+                >
+                    <FontAwesomeIcon icon={faArrowUp} size="2x" />
+                    <span className="text-center w-2/3">Back to Top</span>
+                </a>
             </main>
         </>
     );
